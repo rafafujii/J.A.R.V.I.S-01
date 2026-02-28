d325674c40f36491ee03d3df61966e0e9421018d
Rafael Hiroshi Fujii <134744782+rafafujii@users.noreply.github.com>
2026-02-28 04:42:39 -0300

    Add files via upload

---
+++ Layout JARVIS/agent-starter-react-main/app-config.ts
@@ -0,0 +1,45 @@
+export interface AppConfig {
+  pageTitle: string;
+  pageDescription: string;
+  companyName: string;
+
+  supportsChatInput: boolean;
+  supportsVideoInput: boolean;
+  supportsScreenShare: boolean;
+  isPreConnectBufferEnabled: boolean;
+
+  logo: string;
+  startButtonText: string;
+  accent?: string;
+  logoDark?: string;
+  accentDark?: string;
+
+  // agent dispatch configuration
+  agentName?: string;
+
+  // LiveKit Cloud Sandbox configuration
+  sandboxId?: string;
+}
+
+export const APP_CONFIG_DEFAULTS: AppConfig = {
+  companyName: 'Jarvis',
+  pageTitle: 'Jarvis - Assistente de Voz',
+  pageDescription: 'Um assistente de voz avançado e inteligente.',
+
+  supportsChatInput: true,
+  supportsVideoInput: true,
+  supportsScreenShare: true,
+  isPreConnectBufferEnabled: true,
+
+  logo: '/lk-logo.svg',
+  accent: '#002cf2',
+  logoDark: '/lk-logo-dark.svg',
+  accentDark: '#1fd5f9',
+  startButtonText: 'Iniciar Chamada',
+
+  // agent dispatch configuration
+  agentName: process.env.AGENT_NAME ?? undefined,
+
+  // LiveKit Cloud Sandbox configuration
+  sandboxId: undefined,
+};
